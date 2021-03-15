ca02ae4069770c4c4991067ce0263a47be2d5b27
Marcelo David Barrientos Muñoz <marcebarr241@mesoamericana.edu.gt>
2021-03-14 22:51:32 -0600

    se crean las funciones para escribir en el archivo de texto y para encontrar si hay personas almacenadas o repetidas

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -71,6 +71,7 @@ namespace Pago_de_Propiedades
             this.button1.TabIndex = 3;
             this.button1.Text = "Ingresar";
             this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
             // 
             // Form1
             // 
--- Form1.cs
+++ Form1.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
 using System.Drawing;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,10 @@ namespace Pago_de_Propiedades
 {
     public partial class Form1 : Form
     {
+        List<Propietarios> pro = new List<Propietarios>();
 
-
+        Boolean encontrar_pro = false;
+        int pr = 0;
 
         public Form1()
         {
@@ -24,5 +27,46 @@ namespace Pago_de_Propiedades
         {
 
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        void leer_propietarios()
+        {
+            OpenFileDialog op = new OpenFileDialog();
+            string fileName = "Propietarios.txt";
+            FileStream stream = new FileStream(fileName, FileMode.Open, FileAccess.Read);
+            StreamReader reader = new StreamReader(stream);
+            while (reader.Peek() > -1)
+            {
+                Propietarios p = new Propietarios();
+                p.Dpi = reader.ReadLine();
+                p.Nombre = reader.ReadLine();
+                p.Apellido = reader.ReadLine();
+                pro.Add(p);
+            }
+            reader.Close();
+
+            void repetidos()
+            {
+                while (encontrar_pro == false && pr < pro.Count)
+                {
+                    if (pro[pr].Dpi.CompareTo(textBox1.Text) == 0)
+                    {
+                        encontrar_pro = true;
+                    }
+                    else
+                    {
+                        pr++;
+                    }
+                }
+            }
+
+        }
+
+
+
     }
 }
